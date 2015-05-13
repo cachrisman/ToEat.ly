6b9aa8a283b8706a286ec7316e7b0dc21888a50c
cachrisman <charlie.chrisman@gmail.com>
2015-05-13 16:33:56 -0700

    solution

--- index.js
+++ index.js
@@ -11,42 +11,76 @@ var express = require("express"),
 app.use(express.static(__dirname + '/public'));
 
 // body parser config
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // DATA //
 
 // pre-seeded food data
-var foods =[
-  {id: 0, name: "Sushiritto", yumminess: "quite"},
-  {id: 1, name: "Green Eggs & Ham", yumminess: "sure"},
-  {id: 2, name: "Crayfish", yumminess: "depending"},
-  {id: 3, name: "Foie Gras", yumminess: "omg"},
-  {id: 4, name: "Kale", yumminess: "meh"}
-];
+var foods = [{
+    id: 0,
+    name: "Sushiritto",
+    yumminess: "quite"
+}, {
+    id: 1,
+    name: "Green Eggs & Ham",
+    yumminess: "sure"
+}, {
+    id: 2,
+    name: "Crayfish",
+    yumminess: "depending"
+}, {
+    id: 3,
+    name: "Foie Gras",
+    yumminess: "omg"
+}, {
+    id: 4,
+    name: "Kale",
+    yumminess: "meh"
+}];
 
 // ROUTES //
 
 // root path
-app.get("/", function (req, res){
-  // render index.html
-  res.sendFile(path.join(__dirname + '/public/views/index.html'));
+app.get("/", function(req, res) {
+    // render index.html
+    res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
 // foods index path
-app.get("/foods", function (req, res){
-  // render foods index as JSON
-  res.send(JSON.stringify(foods));
+app.get("/foods", function(req, res) {
+    // render foods index as JSON
+    res.send(JSON.stringify(foods));
 });
 
-app.post("/foods", function (req, res){
-  // food#create
+app.post("/foods", function(req, res) {
+    // food#create
+    // console.log("food#create route is being hit");
+    // console.log(req.body);
+    var newFood = req.body;
+    newFood.id = foods[foods.length - 1].id + 1;
+    foods.push(newFood);
+    res.send(newFood);
 });
 
-app.delete("/foods/:id", function (req, res){
-  // food#delete
+app.delete("/foods/:id", function(req, res) {
+    // food#delete
+    // console.log(foods[req.params.id]);
+    // console.log(req.params.id);
+    pos = foods.map(function(e) {
+      // console.log(e.id);
+        return e.id;
+    }).indexOf(parseInt(req.params.id));
+    // console.log(id_arr);
+    // console.log(typeof req.params.id);
+    // pos = id_arr.indexOf(parseInt(req.params.id));
+    // console.log(pos);
+    // foods.splice(pos, 1);
+    res.send(foods.splice(pos, 1));
 });
 
 // listen on port 3000
-app.listen(3000, function (){
-  console.log("listening on port 3000");
+app.listen(3000, function() {
+    console.log("listening on port 3000");
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,66 @@
 // on page load
-$(function(){
-  // get and render the food
-  Food.all();
+$(function() {
+    // get and render the food
+    Food.all();
+    //set event listeners
+    View.init();
 });
 
 // // // // // // //
 
 // VIEW OBJECT
-function View() {};
+function View() {}
 View.render = function(items, parentId, templateId) {
-  // render a template
-  var template = _.template($("#" + templateId).html());
-  // input data into template and append to parent
-  $("#" + parentId).html(template({collection: items}));
+    // render a template
+    var template = _.template($("#" + templateId).html());
+    // input data into template and append to parent
+    $("#" + parentId).html(template({
+        collection: items
+    }));
 };
 
+View.init = function() {
+    $('#food-form').on("submit", function(event) {
+        event.preventDefault();
+        // console.log($(this).serialize());
+        $.post("/foods", $(this).serialize())
+            .done(function(res) {
+                Food.all();
+                $('#food-form')[0].reset();
+            });
+    });
+    // $('.close').on("click")
+};
+
+View.reset = function() {
+  $('#food-form').off();
+};
 // FOOD OBJECT
-function Food() {};
+function Food() {}
+
 Food.all = function() {
-  $.get("/foods", function(res){ 
-    // parse the response
-    var foods = JSON.parse(res);
-    // render the results
-    View.render(foods, "food-ul", "foods-template");
-  });
+    $.get("/foods", function(res) {
+        // parse the response
+        var foods = JSON.parse(res);
+        // render the results
+        View.render(foods, "food-ul", "foods-template");
+    }).done(function(res){
+      View.reset();
+      View.init();
+    });
+};
+
+Food.delete = function(item) {
+    // console.log(item);
+    // url = "/foods/:" + $(item).attr("data-id");
+    url = "/foods/" + $(item).data().id;
+    // console.log(url);
+    $.ajax({
+        url: url,
+        type: 'DELETE',
+        success: function(response) {
+            console.log(response);
+            Food.all();
+        }
+    });
 };
